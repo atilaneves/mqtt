1d9d36aaa8da309cc778a60a4373f014c049ade2
Atila Neves <aalvesne@cisco.com>
2013-10-31 16:56:41 +0100

    Remaining size decoded correctly

--- mqtt/message.d
+++ mqtt/message.d
@@ -4,22 +4,22 @@ import cerealed.cerealiser;
 import cerealed.decerealiser;
 
 enum MqttType {
-    RESERVED = 0,
-    CONNECT,
-    CONNACK,
-    PUBLISH,
-    PUBACK,
-    PUBREC,
-    PUBREL,
-    PUBCOMP,
-    SUBSCRIBE,
-    SUBACK,
-    UNSUBSCRIBE,
-    UNSUBACK,
-    PINGREQ,
-    PINGRESP,
-    DISCONNECT,
-    RESERVED2
+    RESERVED1   = 0,
+    CONNECT     = 1,
+    CONNACK     = 2,
+    PUBLISH     = 3,
+    PUBACK      = 4,
+    PUBREC      = 5,
+    PUBREL      = 6,
+    PUBCOMP     = 7,
+    SUBSCRIBE   = 8,
+    SUBACK      = 9,
+    UNSUBSCRIBE = 10,
+    UNSUBACK    = 11,
+    PINGREQ     = 12,
+    PINGRESP    = 13,
+    DISCONNECT  = 14,
+    RESERVED2   = 15
 }
 
 struct MqttFixedHeader {
@@ -28,9 +28,9 @@ public:
     bool dup;
     ubyte qos;
     bool retain;
-    ubyte remaining;
+    uint remaining;
 
-    this(MqttType type, bool dup, ubyte qos, bool retain, ubyte remaining = 0) {
+    this(MqttType type, bool dup, ubyte qos, bool retain, uint remaining = 0) {
         this.type = type;
         this.dup = dup;
         this.qos = qos;
@@ -41,21 +41,52 @@ public:
     this(ubyte[] bytes) {
         auto cereal = new Decerealiser(bytes);
         _byte1 = cereal.value!ubyte();
-        remaining = cereal.value!ubyte();
+
         type = cast(MqttType)(_byte1 >> 4);
         dup = cast(bool)(_byte1 & 0x04);
         qos = (_byte1 & 0x06) >> 1;
         retain = cast(bool)(_byte1 & 0x01);
+
+        remaining = getRemainingSize(cereal);
     }
 
     auto encode() const {
         auto cereal = new Cerealiser;
         ubyte byte1 = cast(ubyte)((type << 4) | ((cast(ubyte)dup) << 3) | (qos << 1) | (cast(ubyte)retain));
         cereal ~= byte1;
-        cereal ~= remaining;
+        setRemainingSize(cereal);
         return cereal.bytes;
     }
 
 private:
     ubyte _byte1;
+
+    uint getRemainingSize(Decerealiser cereal) {
+        int multiplier = 1;
+        uint value = 0;
+        ubyte digit;
+        do {
+            digit = cereal.value!ubyte;
+            value += (digit & 127) * multiplier;
+            multiplier *= 128;
+        } while((digit & 128) != 0);
+
+        return value;
+    }
+
+    void setRemainingSize(Cerealiser cereal) const {
+        ubyte[] digits;
+        uint x = remaining;
+        do {
+            ubyte digit = x % 128;
+            x/= 128;
+            if(x > 0) {
+                digit = digit | 0x80;
+            }
+            digits ~= digit;
+        } while(x > 0);
+        foreach(b; digits) {
+            cereal ~= b;
+        }
+    }
 }
--- tests/encode.d
+++ tests/encode.d
@@ -32,3 +32,38 @@ void testDecodeString() {
     const str = MqttString(bytes);
     checkEqual(str.str, "foobarbaz");
 }
+
+void testEncodeBigRemaining() {
+    {
+        const msg = MqttFixedHeader(MqttType.SUBSCRIBE, false, 1, true, 261);
+        checkEqual(msg.type, MqttType.SUBSCRIBE);
+        checkEqual(msg.dup, false);
+        checkEqual(msg.qos, 1);
+        checkEqual(msg.retain, true);
+        checkEqual(msg.remaining, 261);
+        checkEqual(msg.encode(), [0x83, 0x85, 0x02]);
+    }
+    {
+        const msg = MqttFixedHeader(MqttType.SUBSCRIBE, false, 1, true, 321);
+        checkEqual(msg.encode(), [0x83, 0xc1, 0x02]);
+    }
+    {
+        const msg = MqttFixedHeader(MqttType.SUBSCRIBE, false, 1, true, 2_097_157);
+        checkEqual(msg.encode(), [0x83, 0x85, 0x80, 0x80, 0x01]);
+    }
+}
+
+void testDecodeBigRemaining() {
+    {
+        const msg = MqttFixedHeader([0x12, 0xc1, 0x02]);
+        checkEqual(msg.remaining, 321);
+    }
+    {
+        const msg = MqttFixedHeader([0x12, 0x83, 0x02]);
+        checkEqual(msg.remaining, 259);
+    }
+    {
+        const msg = MqttFixedHeader([0x12, 0x85, 0x80, 0x80, 0x01]);
+        checkEqual(msg.remaining, 2_097_157);
+    }
+}
