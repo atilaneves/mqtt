ed538e08087f54aabf722ca8c3e72b6076d0c313
Atila Neves <aalvesne@cisco.com>
2013-11-01 11:28:36 +0100

    Removed useless utf8 class

--- tests/encode.d
+++ tests/encode.d
@@ -1,7 +1,6 @@
 import unit_threaded.check;
 import mqtt.message;
 import mqtt.factory;
-import mqtt.utf8;
 
 
 void testEncodeFixedHeader() {
@@ -21,17 +20,6 @@ void testDecodeFixedHeader() {
     checkEqual(msg.qos, 2);
     checkEqual(msg.retain, false);
     checkEqual(msg.remaining, 5);
-}
-
-void testEncodeString() {
-    const str = MqttString("foobarbaz");
-    checkEqual(str.encode(), [ 0x00, 0x09, 'f', 'o', 'o', 'b', 'a', 'r', 'b', 'a', 'z']);
-}
-
-void testDecodeString() {
-    const ubyte[] bytes = [0x00, 0x09, 'f', 'o', 'o', 'b', 'a', 'r', 'b', 'a', 'z'];
-    const str = MqttString(bytes);
-    checkEqual(str.str, "foobarbaz");
 }
 
 void testEncodeBigRemaining() {
