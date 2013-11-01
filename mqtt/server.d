67c7c16d92e31e5ff947c444cb3389e1d14f6d4d
Atila Neves <aalvesne@cisco.com>
2013-11-01 13:44:38 +0100

    Added test for bad client ID

--- mqtt/server.d
+++ mqtt/server.d
@@ -1,19 +1,36 @@
 module mqtt.server;
 
+
 import mqtt.message;
 import mqtt.factory;
+import std.stdio;
+
 
 abstract class MqttServer {
     void newConnection(MqttConnection connection) {
-        const connack = new MqttConnack(MqttConnack.Code.ACCEPTED);
-        connection.write(connack.encode());
+        const connect = connection.connectMessage;
+        auto code = MqttConnack.Code.ACCEPTED;
+        if(connect.clientId.length < 1 || connect.clientId.length > 23) {
+            code = MqttConnack.Code.BAD_ID;
+        }
+
+        connection.write((new MqttConnack(code)).encode());
     }
 }
 
-interface MqttConnection {
+abstract class MqttConnection {
+    this(in ubyte[] bytes) {
+        connectMessage = cast(MqttConnect)MqttFactory.create(bytes);
+        if(connectMessage is null) {
+            stderr.writeln("Invalid connect message");
+        }
+    }
+
     final void write(in ubyte[] bytes) {
         newMessage(MqttFactory.create(bytes));
     }
 
     void newMessage(MqttMessage msg);
+
+    MqttConnect connectMessage;
 }
--- tests/server.d
+++ tests/server.d
@@ -8,6 +8,7 @@ class TestMqttServer: MqttServer {
 
 class TestMqttConnection: MqttConnection {
     this(in ubyte[] bytes) {
+        super(bytes);
     }
 
     override void newMessage(MqttMessage msg) {
@@ -35,4 +36,50 @@ void testConnect() {
     const connack = cast(MqttConnack)connection.lastMsg;
     checkNotNull(connack);
     checkEqual(connack.code, MqttConnack.Code.ACCEPTED);
+}
+
+
+void testConnectBigId() {
+   auto server = new TestMqttServer();
+    ubyte[] bytes = [ 0x10, 0x29, //fixed header
+                      0x00, 0x06, 'M', 'Q', 'I', 's', 'd', 'p', //protocol name
+                      0x03, //protocol version
+                      0xcc, //connection flags 1100111x username, pw, !wr, w(01), w, !c, x
+                      0x00, 0x0a, //keepalive of 10
+                      0x00, 0x18, 'c', 'i', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd',
+                                  'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', //24 char client id
+                      0x00, 0x04, 'w', 'i', 'l', 'l', //will topic
+                      0x00, 0x04, 'w', 'm', 's', 'g', //will msg
+                      0x00, 0x07, 'g', 'l', 'i', 'f', 't', 'e', 'l', //username
+                      0x00, 0x01, 'p', 'w', //password
+        ];
+
+    auto connection = new TestMqttConnection(bytes);
+    server.newConnection(connection);
+    const connack = cast(MqttConnack)connection.lastMsg;
+    checkNotNull(connack);
+    checkEqual(connack.code, MqttConnack.Code.BAD_ID);
+
+}
+
+void testConnectSmallId() {
+   auto server = new TestMqttServer();
+    ubyte[] bytes = [ 0x10, 0x29, //fixed header
+                      0x00, 0x06, 'M', 'Q', 'I', 's', 'd', 'p', //protocol name
+                      0x03, //protocol version
+                      0xcc, //connection flags 1100111x username, pw, !wr, w(01), w, !c, x
+                      0x00, 0x0a, //keepalive of 10
+                      0x00, 0x00, //no client id
+                      0x00, 0x04, 'w', 'i', 'l', 'l', //will topic
+                      0x00, 0x04, 'w', 'm', 's', 'g', //will msg
+                      0x00, 0x07, 'g', 'l', 'i', 'f', 't', 'e', 'l', //username
+                      0x00, 0x01, 'p', 'w', //password
+        ];
+
+    auto connection = new TestMqttConnection(bytes);
+    server.newConnection(connection);
+    const connack = cast(MqttConnack)connection.lastMsg;
+    checkNotNull(connack);
+    checkEqual(connack.code, MqttConnack.Code.BAD_ID);
+
 }
