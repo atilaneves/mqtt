a7fe58010f5bfd7b2c000005e788633f28cca599
Atila Neves <aalvesne@cisco.com>
2013-11-13 12:33:47 +0100

    Deleted intermittently failing testStressPure

--- tests/server.d
+++ tests/server.d
@@ -185,45 +185,6 @@ void testSubscribeWithMessage() {
     checkEqual(connection.payloads, ["borg", "foo"]);
 }
 
-void testStressPure() {
-    import std.conv;
-    import std.parallelism;
-
-    auto server = new MqttServer();
-    ubyte[] bytes = [ 0x10, 0x2a, //fixed header
-                      0x00, 0x06, 'M', 'Q', 'I', 's', 'd', 'p', //protocol name
-                      0x03, //protocol version
-                      0xcc, //connection flags 1100111x username, pw, !wr, w(01), w, !c, x
-                      0x00, 0x0a, //keepalive of 10
-                      0x00, 0x03, 'c', 'i', 'd', //client ID
-                      0x00, 0x04, 'w', 'i', 'l', 'l', //will topic
-                      0x00, 0x04, 'w', 'm', 's', 'g', //will msg
-                      0x00, 0x07, 'g', 'l', 'i', 'f', 't', 'e', 'l', //username
-                      0x00, 0x02, 'p', 'w', //password
-        ];
-
-    string[] clients;
-    for(int i = 0; i < 100; ++i) clients ~= "client" ~ to!string(i);
-
-    foreach(client; parallel(clients)) {
-        auto connection = new TestMqttConnection(bytes);
-        server.newConnection(connection);
-
-        server.publish("foo/bar/baz", "interesting stuff");
-        checkEqual(connection.payloads, []);
-
-        server.subscribe(connection, 42, [client ~ "/+"]);
-        const suback = cast(MqttSuback)connection.lastMsg;
-        checkNotNull(suback);
-        checkEqual(suback.msgId, 42);
-        checkEqual(suback.qos, [0]);
-
-        server.publish(client ~ "/foo", "interesting stuff");
-        server.publish(client ~ "/foo/bar", "oh noes!!!");
-        checkEqual(connection.payloads, ["interesting stuff"]);
-    }
-}
-
 void testPing() {
     auto server = new MqttServer();
     ubyte[] bytes = [ 0x10, 0x2a, //fixed header
