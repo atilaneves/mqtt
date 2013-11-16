f5df163ef7242cab66aa1ba5d1178aed96250e2d
Atila Neves <aalvesne@cisco.com>
2013-11-16 18:11:25 +0100

    Tried implementing a lock free queue and made everything slower

--- mqttd/broker.d
+++ mqttd/broker.d
@@ -4,6 +4,11 @@ module mqttd.broker;
 import mqttd.message;
 import std.algorithm;
 import std.array;
+import core.atomic;
+import std.traits;
+import std.range;
+import std.functional;
+import std.parallelism;
 
 
 interface MqttSubscriber {
@@ -22,6 +27,52 @@ private bool equalOrPlus(in string pat, in string top) pure nothrow {
 }
 
 
+shared struct FilterQueue(alias F, R) {
+    //alias Unqual!(typeof(R.front())) T;
+
+    private static shared struct Node {
+        Subscription _payload;
+        Node* _next;
+    }
+
+    private Node* _root;
+
+    this(R range) {
+        foreach(item; parallel(range)) {
+            if(F(item)) {
+                pushFront(item);
+            }
+        }
+    }
+
+    void pushFront(Subscription item) {
+        auto newNode = cast(shared)new Node(item, _root);
+        shared(Node)* oldRoot;
+        do {
+            oldRoot = _root;
+            _root = newNode;
+        } while(!cas(&_root, oldRoot, newNode));
+    }
+
+    @property bool empty() { return _root is null; }
+
+    @property auto ref front() {
+        return _root._payload;
+    }
+
+    void popFront() {
+        shared(Node)*oldRoot;
+        do {
+            oldRoot = _root;
+        } while(!cas(&_root, oldRoot, _root._next));
+    }
+}
+
+auto pfilter(alias F, R)(R range) if(isInputRange!(Unqual!R)) {
+    return shared FilterQueue!(unaryFun!F, R)(range);
+}
+
+
 struct MqttBroker {
     void publish(in string topic, in string payload) {
         publish(topic, cast(ubyte[])payload);
@@ -29,8 +80,8 @@ struct MqttBroker {
 
     void publish(in string topic, in ubyte[] payload) {
         const topParts = array(splitter(topic, "/"));
-        foreach(ref s; filter!(s => s.matches(topParts))(_subscriptions)) {
-            s.newMessage(topic, payload);
+        foreach(ref s; pfilter!(s => (cast(shared)s).matches(topParts))(_subscriptions)) {
+            (cast(shared)s).newMessage(topic, payload);
         }
     }
 
@@ -40,25 +91,25 @@ struct MqttBroker {
 
     void subscribe(MqttSubscriber subscriber, in MqttSubscribe.Topic[] topics) {
         foreach(topic; topics) {
-            _subscriptions ~= Subscription(subscriber, topic);
+            _subscriptions ~= shared Subscription(subscriber, topic);
         }
     }
 
     void unsubscribe(MqttSubscriber subscriber) {
-        _subscriptions = std.algorithm.remove!(s => s.isSubscriber(subscriber))(_subscriptions);
+        _subscriptions = std.algorithm.remove!(s => (cast(shared)s).isSubscriber(subscriber))(_subscriptions);
     }
 
     void unsubscribe(MqttSubscriber subscriber, in string[] topics) {
-        _subscriptions = std.algorithm.remove!(s => s.isSubscription(subscriber, topics))(_subscriptions);
+        _subscriptions = std.algorithm.remove!(s => (cast(shared)s).isSubscription(subscriber, topics))(_subscriptions);
     }
 
-    static bool matches(in string topic, in string pattern) {
-        return matches(array(splitter(topic, "/")), pattern);
-    }
+    // static bool matches(in string topic, in string pattern) {
+    //     return matches(array(splitter(topic, "/")), pattern);
+    // }
 
-    static bool matches(in string[] topParts, in string pattern) {
-        return PatternMatcherFactory.create(pattern).matches(topParts);
-    }
+    // static bool matches(in string[] topParts, in string pattern) {
+    //     return cast(shared(PatternMatcher))(PatternMatcherFactory.create(pattern)).matches(topParts);
+    // }
 
 private:
 
@@ -66,10 +117,10 @@ private:
 }
 
 
-private class PatternMatcher {
+private shared class PatternMatcher {
     this(in string topic, in string[] pattern) {
         _topic = topic;
-        _pattern = pattern;
+        _pattern = cast(shared)pattern;
     }
 
     bool isTopic(in string[] topics) const {
@@ -82,7 +133,7 @@ private class PatternMatcher {
     const string[] _pattern;
 }
 
-private class PlusMatcher: PatternMatcher {
+private shared class PlusMatcher: PatternMatcher {
     this(in string topic, in string[] pattern) { super(topic, pattern); }
 
     override bool matches(in string[] topic) const {
@@ -94,7 +145,7 @@ private class PlusMatcher: PatternMatcher {
     }
 };
 
-private class HashMatcher: PatternMatcher {
+private shared class HashMatcher: PatternMatcher {
     this(in string topic, in string[] pattern) { super(topic, pattern); }
 
     override bool matches(in string[] topic) const {
@@ -108,27 +159,27 @@ private class HashMatcher: PatternMatcher {
 }
 
 private class PatternMatcherFactory {
-    static PatternMatcher create(in string topic) {
+    static shared(PatternMatcher) create(in string topic) {
         const pattern = array(splitter(topic, "/"));
         const index = countUntil(pattern, "#");
-        if(index == -1) return new PlusMatcher(topic, pattern);
-        return new HashMatcher(topic, pattern);
+        if(index == -1) return new shared(PlusMatcher)(topic, pattern);
+        return new shared(HashMatcher)(topic, pattern);
     }
 }
 
-private struct Subscription {
+private shared struct Subscription {
     this(MqttSubscriber subscriber, in MqttSubscribe.Topic topic) {
-        _subscriber = subscriber;
+        _subscriber = cast(shared)subscriber;
         _matcher = PatternMatcherFactory.create(topic.topic);
         _qos = topic.qos;
     }
 
-    bool matches(in string[] topic) const {
+    shared bool matches(in string[] topic) const {
         return _matcher.matches(topic);
     }
 
     void newMessage(in string topic, in ubyte[] payload) {
-        _subscriber.newMessage(topic, payload);
+        (cast(MqttSubscriber)_subscriber).newMessage(topic, payload);
     }
 
     bool isSubscriber(MqttSubscriber subscriber) const {
