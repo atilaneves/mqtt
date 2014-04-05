b16ea90146b25b4181a2a22d92c1a04468da8432
Atila Neves <aalvesne@cisco.com>
2014-04-05 15:59:07 +0200

    Replaced switch in factory with associative array of message creator functions

--- mqttd/factory.d
+++ mqttd/factory.d
@@ -7,6 +7,21 @@ import std.stdio;
 
 
 struct MqttFactory {
+    static this() {
+        with(MqttType) {
+            registerType!MqttConnect(CONNECT);
+            registerType!MqttConnack(CONNACK);
+            registerType!MqttPublish(PUBLISH);
+            registerType!MqttSubscribe(SUBSCRIBE);
+            registerType!MqttSuback(SUBACK);
+            registerType!MqttUnsubscribe(UNSUBSCRIBE);
+            registerType!MqttUnsuback(UNSUBACK);
+            registerType!MqttPingReq(PINGREQ);
+            registerType!MqttPingResp(PINGRESP);
+            registerType!MqttDisconnect(DISCONNECT);
+        }
+    }
+
     static MqttMessage create(in ubyte[] bytes) {
 
         auto cereal = Decerealiser(bytes);
@@ -18,30 +33,21 @@ struct MqttFactory {
 
         cereal.reset(); //so that the created MqttMessage can re-read the header
 
-        switch(fixedHeader.type) with(MqttType) {
-        case CONNECT:
-            return cereal.value!MqttConnect(fixedHeader);
-        case CONNACK:
-            return cereal.value!MqttConnack(fixedHeader);
-        case PUBLISH:
-            return cereal.value!MqttPublish(fixedHeader);
-        case SUBSCRIBE:
-            return cereal.value!MqttSubscribe(fixedHeader);
-        case SUBACK:
-            return cereal.value!MqttSuback(fixedHeader);
-        case UNSUBSCRIBE:
-            return cereal.value!MqttUnsubscribe(fixedHeader);
-        case UNSUBACK:
-            return cereal.value!MqttUnsuback(fixedHeader);
-        case PINGREQ:
-            return cereal.value!MqttPingReq(fixedHeader);
-        case PINGRESP:
-            return cereal.value!MqttPingResp(fixedHeader);
-        case DISCONNECT:
-            return cereal.value!MqttDisconnect(fixedHeader);
-        default:
+        if(fixedHeader.type !in _msgCreators) {
             stderr.writeln("Unknown MQTT message type: ", fixedHeader.type);
             return null;
         }
+
+        return _msgCreators[fixedHeader.type](fixedHeader, cereal);
+    }
+
+private:
+
+    static MqttMessage function(MqttFixedHeader, Decerealiser)[MqttType] _msgCreators;
+
+    static void registerType(T)(MqttType type) {
+        _msgCreators[type] = (header, cereal) {
+            return cereal.value!T(header);
+        };
     }
 }
