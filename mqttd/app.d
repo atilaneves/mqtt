28732f5e9b1b866973f3ebf4b73a8a8b7871b184
Atila Neves <atila.neves@gmail.com>
2015-11-15 08:28:09 +0100

    Vibe custom main

--- mqttd/app.d
+++ mqttd/app.d
@@ -24,3 +24,42 @@ void accept(TCPConnection tcpConnection) {
     mqttConnection.run(gServer);
     if(tcpConnection.connected) tcpConnection.close();
 }
+
+
+int main()
+{
+    import vibe.core.args : finalizeCommandLineOptions;
+    import vibe.core.core : runEventLoop, lowerPrivileges;
+    import vibe.core.log;
+    import std.encoding : sanitize;
+
+    version (unittest) {
+        logInfo("All unit tests were successful.");
+        return 0;
+    } else {
+        try if (!finalizeCommandLineOptions()) return 0;
+        catch (Exception e) {
+            logDiagnostic("Error processing command line: %s", e.msg);
+            return 1;
+        }
+
+        lowerPrivileges();
+
+        logDiagnostic("Running event loop...");
+        int status;
+        version (VibeDebugCatchAll) {
+            try {
+                status = runEventLoop();
+            } catch( Throwable th ){
+                logError("Unhandled exception in event loop: %s", th.msg);
+                logDiagnostic("Full exception: %s", th.toString().sanitize());
+                return 1;
+            }
+        } else {
+            status = runEventLoop();
+        }
+
+        logDiagnostic("Event loop exited with status %d.", status);
+        return status;
+    }
+}
